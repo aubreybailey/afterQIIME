47bde27408f93c68dee2841e6f801a2580e77e8d
Aubrey Bailey <aubreybailey@gmail.com>
2016-12-07 17:24:38 -0500

    moved everyhting into R

---
+++ R/test_OTU_abundance.R
@@ -0,0 +1,63 @@
+#' Test OTUs for differences in abundance
+#' @param s A data frame of sample info.
+#' @param cts A matrix of OTU counts (rows) for each sample (columns).
+#' @param g A factor of sample groups to be tested.
+#' @param a A chearacter vector of OTU assignments.
+test_otu_abundance <- function (s, cts, var="study_group", a=NULL, min_fraction=0.5) {
+  # Limit OTU table to samples in provided data frame
+  cts <- cts[, s$SampleID]
+  
+  # Matrix of OTU proportions
+  props <- sweep(cts, 2, colSums(cts), `/`)
+
+  # Empty result
+  res <- list()
+
+  # Detect OTUs present in < 5 samples
+  frac_present <- function (x) sum(x > 0) / length(x)
+  too_rare <- apply(cts, 1, function (x) frac_present(x) < min_fraction)
+
+  res$tests <- apply(cts[!too_rare,], 1, function (x) kruskal.test(x,s[[var]]))
+
+  res$df <- data.frame(
+    OtuID = names(res$tests),
+    Taxon = a[names(res$tests)],
+    Stat = sapply(res$tests, `[[`, "statistic"),
+    Pval = sapply(res$tests, `[[`, "p.value"))
+  res$df <- within(res$df, {
+    Fdr <- p.adjust(Pval, method="fdr")
+  })
+  res$df <- arrange(res$df, Pval)
+  
+  plot_otus <- as.character(subset(res$df, Pval < 0.05)$OtuID)
+  if (length(plot_otus) > 20) {
+    plot_otus <- plot_otus[1:20]
+  }
+  
+  plotdf <- melt(props[plot_otus,], varnames=c("OtuID", "SampleID"), value.name="Proportion")
+  plotdf <- merge(plotdf, res$df, by="OtuID", all.x=T, all.y=F)
+  plotdf <- merge(plotdf, s, by="SampleID", all.x=T, all.y=F)
+  plotdf <- within(plotdf, {
+    Label <- factor(paste(OtuID, Taxon))
+  })
+  plotdf$Label <- reorder(plotdf$Label, plotdf$Pval)
+
+  res$plot <- ggplot(plotdf) + 
+    geom_boxplot(aes_string(x=var, y="Proportion")) +
+    facet_wrap(~ Label, scales="free_y", ncol=2) +
+    theme_classic() + 
+    theme(
+      strip.text.x = element_text(size = 8), 
+      strip.text.y = element_text(size = 8))
+
+  class(res) <- "otu_abundance_tests"
+  res
+}
+print.otu_abundance_tests <- function (x) {
+  print(x$plot)
+  df <- subset(x$df, Pval < 0.05)
+  if (nrow(df) > 20) {
+    df <- df[1:20,]
+  }
+  print(df)
+}
